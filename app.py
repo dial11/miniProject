0c161ccb5a95950751e6261ac37170da991eba29
junhyuk <wwns1009.gmail.com>
2022-11-18 02:46:55 +0900

    s

--- app.py
+++ app.py
@@ -10,18 +10,32 @@ def p2home():
    return render_template('p2.html')
 
 @app.route("/p2_comment", methods=["POST"])
-def comment_post():
+def p2comment_post():
     comment_receive = request.form['comment_give']
+
+    comment_list = list(db.p2_comment.find({}, {'_id': False}));
+    count = len(comment_list) + 1
+
     doca = {
         'comment': comment_receive,
+        'num': count
     }
     db.p2_comment.insert_one(doca)
     return jsonify({'msg':'입력 완료!'})
 
 @app.route("/p2_comment", methods=["GET"])
-def comment_get():
+def p2comment_get():
     comment_list = list(db.p2_comment.find({}, {'_id': False}))
     return jsonify({'comment':comment_list})
+
+@app.route('/p2_delComment', methods=['POST'])
+def p2_comment_delete():
+    num_receive = request.form['num_give']
+
+    db.p2_comment.delete_one({'num': int(num_receive)});
+
+    return jsonify({'result': 'success', 'msg': '삭제 완료!'})
+
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
